399e5c6814113dab6277fc0a150cb3c357c65cc6
UsmanS2 <usman.shafi@theaxlegroup.com>
2024-10-26 18:30:29 -0500

    property manager landing finished

--- src/components/PMHero/PMHero.tsx
+++ src/components/PMHero/PMHero.tsx
@@ -22,37 +22,11 @@ export function PMHero() {
             Located in Dallas, Texas
           </Text>
 
-          {/* <List
-            mt={30}
-            spacing="sm"
-            size="sm"
-            icon={
-              <ThemeIcon size={20} radius="xl">
-                <IconCheck style={{ width: rem(12), height: rem(12) }} stroke={1.5} />
-              </ThemeIcon>
-            }
-          >
-            <List.Item>
-              <b>TypeScript based</b> – build type safe applications, all components and hooks
-              export types
-            </List.Item>
-            <List.Item>
-              <b>Free and open source</b> – all packages have MIT license, you can use Mantine in
-              any project
-            </List.Item>
-            <List.Item>
-              <b>No annoying focus ring</b> – focus ring will appear only when user navigates with
-              keyboard
-            </List.Item>
-          </List> */}
-
           <Group mt={30}>
             <Button radius="xl" size="md" className={classes.control}>
               View Details →
             </Button>
-            {/* <Button variant="default" radius="xl" size="md" className={classes.control}>
-              Source code
-            </Button> */}
+
           </Group>
           </div>
           <RingProgress
@@ -62,7 +36,31 @@ export function PMHero() {
           label={<Text size="xl" ta="center">70%</Text>}
         />
         
-        {/* <Image src={image.src} className={classes.image} /> */}
+      </div>
+
+      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between' }} className={classes.inner}>
+        <div className={classes.content}>
+          <Title className={classes.title}>
+            <span className={classes.highlight}>Building 2</span> 
+          </Title>
+          <Text c="dimmed" mt="md">
+            Located in Los Angelos, California
+          </Text>
+
+          <Group mt={30}>
+            <Button radius="xl" size="md" className={classes.control}>
+              View Details →
+            </Button>
+
+          </Group>
+          </div>
+          <RingProgress
+          size={250}
+          thickness={24}
+          sections={[{ value: 90, color: 'green' }]} 
+          label={<Text size="xl" ta="center">90%</Text>}
+        />
+        
       </div>
     </Container>
   );
